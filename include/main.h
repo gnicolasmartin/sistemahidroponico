a79b658c6b5755570c403d11c31195f4eb0b55ed
santylaborde <santylaborde@hotmail.com>
2021-10-25 21:21:22 -0300

    main con ultimo merge

--- include/main.h
+++ include/main.h
@@ -1,9 +1,29 @@
 #include <stdio.h>
+
 #include "esp_system.h"
 #include "nvs_flash.h"
+/////////////////////////////////////////////////////////////////////////////////////////
+//  Proyecto: Sistema Hidropónico Para Hogares                                         // 
+//  Alumnos: *Lucia Sucunza                                                            //
+//           *Nicolas Gomez                                                            //
+//           *Santiago Laborde                                                         //
+//                                                                                     // 
+//  Archivo: lcd_i2c.h                                                                 // 
+//  Descripción: Archivo de cabecera para el main                                      //
+//                                                                                     // 
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// INCLUDES
 #include "tasks.h"
-#include "wifi_mqtt.h"
-#include "funciones_json.h"
+#include "wifi.h"
+#include "filesystem.h"
+
+// PROTOTYPES
+extern TaskHandle_t task_handler_1;
+extern TaskHandle_t task_handler_2;
+extern TaskHandle_t task_handler_3;
+extern TaskHandle_t task_handler_4;
+extern TaskHandle_t task_handler_5;
 
 
 
--- src/main.c
+++ src/main.c
@@ -10,10 +10,14 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 
 #include "main.h"
-#include "esp_partition.h"
-#include "esp_spiffs.h"
 
-extern TaskHandle_t task_handle_1, task_handle_2, task_handle_3, task_handle_4, task_handle_5;
+esp_vfs_spiffs_conf_t conf = 
+{
+    .base_path = "/spiffs",
+    .partition_label = NULL,
+    .max_files = 5,
+    .format_if_mount_failed = true
+};
 
 void app_main()
 {
@@ -22,20 +26,28 @@ void app_main()
     init_gpio();
     adc_init();
     lcd_Init();
+    init_wifi();
+    wifi_wait();
+    init_fs(&conf);
 
     lcd_SendString("Bienvenido\\a", LCD_ROW_1);
 	lcd_SendString("Ingresar al menú->", LCD_ROW_2);
 
     //Se crean las tareas a despachar por el scheduler
-    xTaskCreate(&toggle_led, "toggle_led", 1024, NULL, 1, &task_handle_1);
-    xTaskCreate(&leer_entradas, "leer_entradas", 1024, NULL, 1, &task_handle_2);
-    xTaskCreate(&leer_adc_ec, "leer_adc_ec", 4096, NULL, 2, &task_handle_3);
-    xTaskCreate(&navegar_menu, "navegar_menu", 4096, NULL, 2, &task_handle_4);
-    xTaskCreate(&control_lcd, "control_lcd", 4096, NULL, 2, &task_handle_5);    
+    xTaskCreate(&toggle_led, "toggle_led", 1024, NULL, 1, &task_handler_1);
+    xTaskCreate(&leer_entradas, "leer_entradas", 1024, NULL, 1, &task_handler_2);
+    xTaskCreate(&leer_adc_ec, "leer_adc_ec", 4096, NULL, 2, &task_handler_3);
+    xTaskCreate(&navegar_menu, "navegar_menu", 4096, NULL, 2, &task_handler_4);
+    xTaskCreate(&control_lcd, "control_lcd", 4096, NULL, 2, &task_handler_5);    
     xTaskCreate(&toggle_pin,"toggle_pin", 4096, NULL, 1, NULL);
+    xTaskCreate(&firestore_task,"firestore", 10240, NULL, 4, NULL);
     //xTaskCreate(&tarea2, "tarea2", 1024, NULL, 2, NULL);
     //xTaskCreate(&tarea3, "tarea3", 1024, NULL, 3, NULL);
     //xTaskCreate(&tarea4, "tarea4", 1024, NULL, 4, NULL);
 
+    // All done, unmount partition and disable SPIFFS
+    // esp_vfs_spiffs_unregister(conf.partition_label);
+    // printf("SPIFFS unmounted\n");
+    
     //vTaskStartScheduler();
 }
